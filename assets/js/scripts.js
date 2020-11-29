3df181e3a5c5c0de8319d6a2521f8419729518a8
Danil Vorozheykin <danilvda@gmail.com>
2020-11-29 17:50:09 +0300

    Fix some bugs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,58 +1,56 @@
 'use strict'
 
-$(function () {
-	// Navbar
-	let navbar = $('#topnav')
+// Navbar
+let navbar = $('#navbar')
 
-	$('.navbar-toggle').on('click', function () {
-		$(this).toggleClass('open')
-		$('#navigation').slideToggle(400)
-	})
+$('.navbar-toggle').on('click', function () {
+	$(this).toggleClass('open')
+	$('#navigation').slideToggle(400)
+})
 
-	document.querySelectorAll('.navigation-menu a[href^="#"]').forEach(function (anchor) {
-		anchor.addEventListener('click', function (e) {
-			e.preventDefault()
+document.querySelectorAll('.navigation-menu a[href^="#"]').forEach(function (anchor) {
+	anchor.addEventListener('click', function (e) {
+		e.preventDefault()
 
-			document.querySelector(this.getAttribute('href')).scrollIntoView({
-				behavior: 'smooth'
-			})
+		document.querySelector(this.getAttribute('href')).scrollIntoView({
+			behavior: 'smooth'
 		})
 	})
+})
 
-	$(window).scroll(function () {
-		let scroll = $(window).scrollTop()
+$(window).scroll(function () {
+	if ($(window).scrollTop() >= 50) {
+		navbar.addClass('nav-sticky')
+	} else {
+		navbar.removeClass('nav-sticky')
+	}
+})
 
-		if (scroll >= 50) {
-			navbar.addClass('nav-sticky')
-		} else {
-			navbar.removeClass('nav-sticky')
-		}
+// Header image change
+let images = ['graph.svg', 'key-value.svg', 'documents.svg'],
+	slider = $('#img-slider'),
+	index = 0,
+	maxImages = images.length - 1
+setInterval(function () {
+	let currentImage = images[index]
+	index = (index === maxImages) ? 0 : ++index
+	slider.fadeOut(500, function () {
+		slider.attr('src', 'assets/img/' + currentImage)
+		slider.fadeIn(500)
 	})
+}, 5000)
 
-	// Header image change
-	let images = ['graph.svg', 'key-value.svg', 'documents.svg'],
-		slider = $('#img-slider'),
-		index = 0,
-		maxImages = images.length - 1
-	setInterval(function () {
-		let currentImage = images[index]
-		index = (index === maxImages) ? 0 : ++index
-		slider.fadeOut(500, function () {
-			slider.attr('src', 'assets/img/' + currentImage)
-			slider.fadeIn(500)
-		})
-	}, 5000)
+// Back to top
 
-	// Back to top
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 100) {
-			$('.back-to-top').fadeIn()
-		} else {
-			$('.back-to-top').fadeOut()
-		}
-	})
-	$('.back-to-top').click(function () {
-		$('html, body').animate({scrollTop: 0}, 1000)
-		return false
-	})
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 100) {
+		$('.back-to-top').fadeIn()
+	} else {
+		$('.back-to-top').fadeOut()
+	}
+})
+
+$('.back-to-top').click(function () {
+	$('html, body').animate({scrollTop: 0}, 1000)
+	return false
 })
